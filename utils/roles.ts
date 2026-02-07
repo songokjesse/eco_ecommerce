d0f7ffc0938fed1eaf2c0572abfacf17b053cb87
Jesse Songok <jessesongok@gmail.com>
2026-02-07 15:45:03 +0800

    fix: Add optional chaining for safer sessionClaims metadata access.

--- utils/roles.ts
+++ utils/roles.ts
@@ -2,5 +2,5 @@ import { auth } from '@clerk/nextjs/server';
 
 export const checkRole = async (role: 'admin' | 'seller') => {
     const { sessionClaims } = await auth();
-    return sessionClaims?.metadata.role === role;
+    return sessionClaims?.metadata?.role === role;
 };
